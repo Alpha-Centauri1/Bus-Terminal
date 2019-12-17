1fe99935f9d2f2ecb07fced4533ab5eeede6df3c
Gogo Magnus <gogomagnusprinceps@gmail.com>
2019-12-17 14:27:27 +0100

    Created properties and methods for Station class.

---
+++ Bus Terminal/src/main/java/Station.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Station {
+    private String name;
+    private List<String> terminals;
+
+    public Trip arrive(Trip trip){
+
+        return trip;
+    }
+
+    public Integer departure(String terminalNumber){
+
+        return 4;
+    }
+
+    public Integer terminalStatus(String terminalNumber){
+
+        return 1;
+    }
+}

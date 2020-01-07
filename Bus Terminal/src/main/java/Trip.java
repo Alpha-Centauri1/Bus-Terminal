6298d80f9978d4bbce2a1885aa0ca5b7667f84e0
NoahSN <honor10noah@gmail.com>
2020-01-07 14:10:46 +0100

    Created Trip with functions and variables

---
+++ Bus Terminal/src/main/java/Trip.java
@@ -0,0 +1,28 @@
+package main.java;
+
+public class Trip {
+    private int number;
+    private String departureTime;
+    private String arrivalTime;
+    private Bus bus;
+
+
+    public Trip(){
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public String getDepartureTime(){
+        return departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public Bus getBus(){
+        return Bus;
+    }
+}

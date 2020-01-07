019f11236f57c8f610f7a7ff603002c842cf4273
Alpha-Centauri1 <luca@tuninga.com>
2020-01-07 14:35:14 +0100

    Added constructor and getter, setter

--- Bus Terminal/src/main/java/Bus.java
+++ Bus Terminal/src/main/java/Bus.java
@@ -2,4 +2,25 @@ public class Bus {
 
     private int capacity;
     private int type;
+
+    public Bus(Integer capacity, Integer type) {
+        this.capacity = capacity;
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
 }

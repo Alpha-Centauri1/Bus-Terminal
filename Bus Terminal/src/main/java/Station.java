663360367e3980a01fd16413221c1661e55d5d2d
Gogo Magnus <gogomagnusprinceps@gmail.com>
2020-01-07 16:31:01 +0100

    arrival method in progress.

--- Bus Terminal/src/main/java/Station.java
+++ Bus Terminal/src/main/java/Station.java
@@ -2,21 +2,34 @@ import java.util.List;
 
 public class Station {
     private String name;
-    private List<String> terminals;
+    private List<Terminal> terminals;
 
-    public Station(){
-
-    }
-
-    public int arrive(Trip trip){
-        return 1;
-    }
-
-    public void departure(String terminalNumber){
+    public Station() {
 
     }
 
-    public void terminalStatus(String terminalNumber){
+    public int arrive(Trip trip) {
+        for (int i = 0; i < terminals.size(); i++) {
+            if (terminals.get(i).isAvailable()) {
+                if (trip.isNational()) {
+                    terminals.get(i).arrive(trip);
+                    return i + 1;
+                }else {
+                    if (trip.getBus().isBig()){
+
+                    }
+                }
+
+            }
+        }
+        return -1;
+    }
+
+    public void departure(String terminalNumber) {
+
+    }
+
+    public void terminalStatus(String terminalNumber) {
 
     }
 }

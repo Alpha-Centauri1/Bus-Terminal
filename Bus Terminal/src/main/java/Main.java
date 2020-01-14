19a4663b5a62488719513a5da4214c30932bea18
Gogo Magnus <gogomagnusprinceps@gmail.com>
2020-01-14 16:16:36 +0100

    created main class

--- Bus Terminal/src/main/java/Main.java
+++ Bus Terminal/src/main/java/Main.java
@@ -4,9 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("  Menu  \n========\n\n1)Arrive\n2)Depart\n3)Terminal-Information\n\n4)Exit");
-        String input = scanner.next();
+        String input = scanner.nextLine();
 
         switch (input){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                System.exit(0);
+                break;
+                default:
+                    System.out.println("Invalid Input!");
+                    break;
 
         }
     }

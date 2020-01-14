116acfc0ed09895ea649efd2aeebbe763040d90b
Gogo Magnus <gogomagnusprinceps@gmail.com>
2020-01-14 15:31:04 +0100

    created main class

---
+++ Bus Terminal/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("  Menu  \n========\n\n1)Arrive\n2)Depart\n3)Terminal-Information\n\n4)Exit");
+        String input = scanner.next();
+
+        switch (input){
+
+        }
+    }
+}

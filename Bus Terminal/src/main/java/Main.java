92c301567247ceffd9382f4b432a25210844b62c
Gogo Magnus <gogomagnusprinceps@gmail.com>
2020-01-21 13:32:28 +0100

    Added menu and json-file

--- Bus Terminal/src/main/java/Main.java
+++ Bus Terminal/src/main/java/Main.java
@@ -3,23 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("  Menu  \n========\n\n1)Arrive\n2)Depart\n3)Terminal-Information\n\n4)Exit");
-        String input = scanner.nextLine();
+        String input = "";
 
-        switch (input){
-            case "1":
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
-            case "4":
-                System.exit(0);
-                break;
+        do {
+            System.out.println("  Menu  \n========\n\n1)Arrive\n2)Depart\n3)Terminal-Information\n\n4)Exit");
+            input = scanner.nextLine();
+
+            switch (input) {
+                case "1":
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    System.exit(0);
+                    break;
                 default:
                     System.out.println("Invalid Input!");
                     break;
 
-        }
+            }
+        } while (!input.equals("4"));
+
     }
 }
